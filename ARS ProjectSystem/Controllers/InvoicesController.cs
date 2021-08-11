7da009aa0febf52d4cdbb2a1524ff23470afce67
AnetaKoseva <62696878+AnetaKoseva@users.noreply.github.com>
2021-08-11 14:23:40 +0300

    Edit Invoice is implemented and works property

--- ARS ProjectSystem/Controllers/InvoicesController.cs
+++ ARS ProjectSystem/Controllers/InvoicesController.cs
@@ -5,12 +5,9 @@
     using ARS_ProjectSystem.Models.Invoices;
     using Microsoft.AspNetCore.Authorization;
     using Microsoft.AspNetCore.Mvc;
-    using System;
-    using System.Globalization;
     using System.Linq;
 
-
-
+    using static WebConstants;
     public class InvoicesController:Controller
     {
         private readonly ProjectSystemDbContext data;
@@ -84,7 +81,7 @@
         }
 
         [Authorize]
-        public IActionResult Add(int id, string registrationNumber)
+        public IActionResult Add(int id)
         {
             var invoice = this.data.Invoices.FirstOrDefault(i=>i.Id==id);
             
@@ -109,9 +106,78 @@
             return View(model);
         }
         [Authorize]
+        public IActionResult Edit(int id)
+        {
+            var invoices = this.data.Invoices.Where(i => i.Id == id)
+                .Select(i => new InvoiceFormModel
+                {
+                    Id = i.Id,
+                    Number = i.Number,
+                    Item = i.Item,
+                    CreatedOn = i.CreatedOn,
+                    DueDate = i.DueDate,
+                    CustomerRegistrationNumber = i.CustomerRegistrationNumber,
+                    CustomerVAT = i.CustomerVAT,
+                    CustomerAddress = i.CustomerAdress,
+                    CustomerOwner = i.CustomerOwnerName,
+                    Quantity = i.Quantity,
+                    CustomerName = i.CustomerName,
+                    Country = i.CustomerCountry,
+                    Town = i.CustomerTown,
+                    Price = i.Price,
+                    Total = i.Total
+                }).FirstOrDefault();
+            return View(new InvoiceFormModel
+            {
+                Id = invoices.Id,
+                Number = invoices.Number,
+                Item = invoices.Item,
+                CreatedOn = invoices.CreatedOn,
+                DueDate = invoices.DueDate,
+                CustomerRegistrationNumber = invoices.CustomerRegistrationNumber,
+                CustomerVAT = invoices.CustomerVAT,
+                CustomerAddress = invoices.CustomerAddress,
+                CustomerOwner = invoices.CustomerOwner,
+                Quantity = invoices.Quantity,
+                CustomerName = invoices.CustomerName,
+                Country = invoices.Country,
+                Town = invoices.Town,
+                Price = invoices.Price,
+                Total = invoices.Total
+            });
+
+        }
+        [Authorize]
+        [HttpPost]
+        public IActionResult Edit(InvoiceFormModel invoice,int id)
+        {
+            var invoiceData = this.data
+                .Invoices
+                .Find(id);
+
+            invoiceData.Item = invoice.Item;
+            invoiceData.Number = invoice.Number;
+            invoiceData.Price = invoice.Price;
+            invoiceData.Quantity = invoice.Quantity;
+            invoiceData.CreatedOn = invoice.CreatedOn;
+            invoiceData.DueDate = invoice.DueDate;
+            invoiceData.CustomerOwnerName = invoice.CustomerOwner;
+            invoiceData.CustomerAdress = invoice.CustomerAddress;
+            invoiceData.CustomerCountry = invoice.Country;
+            invoiceData.CustomerTown = invoice.Town;
+
+            this.data.SaveChanges();
+
+            TempData[GlobalMessageKey] = $"You invoice {invoice.Id} is edited!";
+
+            return RedirectToAction(nameof(All));
+        }
+        [Authorize]
         public IActionResult Delete(int id)
         {
-            var invoice= this.data.Invoices.FirstOrDefault(i => i.Id == id);
+            var invoice= this.data
+                .Invoices
+                .FirstOrDefault(i => i.Id == id);
             this.data.Invoices.Remove(invoice);
             this.data.SaveChanges();
             return RedirectToAction(nameof(All));

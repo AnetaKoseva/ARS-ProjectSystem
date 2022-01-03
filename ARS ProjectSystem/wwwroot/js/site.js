67ecd99690136fe1e16f5b4cabe1b39c22bb7161
AnetaKoseva <62696878+AnetaKoseva@users.noreply.github.com>
2022-01-03 12:47:00 +0200

    PAge views and recaptcha

--- ARS ProjectSystem/wwwroot/js/site.js
+++ ARS ProjectSystem/wwwroot/js/site.js
@@ -52,8 +52,6 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
     // Activate SimpleLightbox plugin for portfolio items
-    new SimpleLightbox({
-        elements: '#portfolio a.portfolio-box'
-    });
+
 
 });
